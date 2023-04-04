9bb214d16818d6d5c29287e345d82bade2549d04
abbabe <dagistan18@gmail.com>
2023-04-04 15:26:08 +0000

    some changes

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -3,12 +3,14 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloController {
+    private String message = "Greetings from Spring Boot! HELLO GALAXY :)";
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return message;
 	}
 
 }
