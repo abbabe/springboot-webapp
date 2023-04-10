eed1144a36e22dbbd853c813dc77abaf7d5cdd8b
abbabe <dagistan18@gmail.com>
2023-04-10 12:15:20 +0000

    some fixes

--- complete/jenkins/build-docker-image.sh
+++ complete/jenkins/build-docker-image.sh
@@ -1,5 +1,6 @@
 export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
 export PATH=$JAVA_HOME/bin:$PATH
 echo $JAVA_HOME
+sleep 30000
 $HOME/springboot-webapp/complete/mvnw clean package
 docker build --force-rm -t "abbabe/springboot" $HOME/springboot-webapp/complete
